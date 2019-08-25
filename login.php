1ef0d886b22465ca4549656de3b880e01dc7d3f9
matheusmo <matheus.oliveira@innovareti.com.br>
2019-08-25 20:54:28 -0300

    Pacotes

--- login.php
+++ login.php
@@ -5,18 +5,15 @@
     $user = $_POST['userName'];
     $password = $_POST['password'];
 
-    $con = mysqli_connect("localhost", "root", "admin", "Servers");
+    $con = mysqli_connect("localhost", "root", "admin", "Server");
 
-    $result = mysqli_query($con,"SELECT * FROM Usuario WHERE user = '" . $user . "' AND password = '".$password."'");
+    $result = mysqli_query($con,"SELECT * FROM Usuario WHERE user = '" . $user . "' AND password = '".$password."';");
 
-    var_dump($result);
-
-    if($result){
+    if(mysqli_num_rows($result) > 0){
         $_SESSION['userName'] = $user;
         $_SESSION['password'] = $password;
         header('location:crud.php');
     }else{
-        echo ("</br>6");
         unset ($_SESSION['userName']);
         unset ($_SESSION['password']);
         header('location:index.php');
